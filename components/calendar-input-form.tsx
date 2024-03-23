293822f5514b861fc4a51c518ee00f0df1078e48
desarrollador-hseq <desarrolladorhseq@hotmail.com>
2024-03-23 11:33:52 -0500

    se agrega para seleccionar el año y el mes desde un select en el calendar input

--- components/calendar-input-form.tsx
+++ components/calendar-input-form.tsx
@@ -72,6 +72,10 @@ export const CalendarInputForm: React.FC<InputFormProps<any>> = ({
                 onSelect={field.onChange}
                 disabled={((date) => date < new Date("1900-01-01")) || disabled}
                 initialFocus
+                fromYear={2010}
+                toYear={2040}
+                captionLayout="dropdown-buttons"
+
               />
             </PopoverContent>
           </Popover>
